4cf715deffd0e0939b7083eeff0c06f585990c83
Michael <mkozubski@academic.rrc.ca>
2023-09-11 20:51:55 -0500

    Changed podcast api, to https://taddy.org/.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("io.github.yusufyilmazfr:podcast4j:1.0.0")
+    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/podcastapi/MainActivity.java
+++ app/src/main/java/com/example/podcastapi/MainActivity.java
@@ -1,75 +1,153 @@
 package com.example.podcastapi;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String API_KEY = "69a27af18f0848d6968210073133948e";
-    private static final String API_URL = "https://listen-api.listennotes.com/api/v2/search?q=SEARCH_QUERY";
+    private static final String TAG = "TaddyPodcastAPI";
+    private static final String API_URL = "https://api.taddy.org";
+    private static final String X_USER_ID = "599";
+    private static final String X_API_KEY = "bfc65097269c742516dad0f0f2da4d4e1958eda6cadabc576d44999f512012649e41897fbb21449decf977c1d4f1b14663"; // Replace with your actual API key
+    private TextView podcastNameTextView;
+    private EditText podcastQueryEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.submit(() -> {
-            String response = fetchPodcasts("Technology");
-            runOnUiThread(() -> {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray jsonArray = jsonObject.getJSONArray("results");
-                    StringBuilder podcastTitles = new StringBuilder();
+        // Find views by their IDs
+        Button searchButton = findViewById(R.id.searchButton);
+        podcastNameTextView = findViewById(R.id.podcastNameTextView);
+        podcastQueryEditText = findViewById(R.id.podcastQueryEditText);
 
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject podcastObject = jsonArray.getJSONObject(i);
-                        String title = podcastObject.getString("title_original");
-                        podcastTitles.append(title).append("\n");
-                    }
-
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(podcastTitles.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            });
+        // Set the click listener for the searchButton
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchPodcastData();
+            }
         });
     }
 
+    public void onSearchClick(View view) {
+        fetchPodcastData();
+    }
 
-    private String fetchPodcasts(String searchQuery) {
-        String response = "";
+    public void fetchPodcastData() {
+        // Get the user's podcast query from the EditText field
+        String podcastQuery = podcastQueryEditText.getText().toString();
 
-        try {
-            URL url = new URL(API_URL.replace("SEARCH_QUERY", searchQuery));
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("X-ListenAPI-Key", API_KEY);
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoInput(true);
+        // Trigger the FetchPodcastDataTask with the user's query
+        new FetchPodcastDataTask().execute(podcastQuery);
+    }
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response += line;
+    private class FetchPodcastDataTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            if (params.length != 1) {
+                return null; // Return early if no query provided
             }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            String podcastQuery = params[0];
+
+            try {
+                // Create the API URL
+                URL url = new URL(API_URL);
+
+                // Open a connection to the URL
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("X-USER-ID", X_USER_ID);
+                connection.setRequestProperty("X-API-KEY", X_API_KEY);
+                connection.setDoOutput(true);
+
+                // Construct the JSON request body with the user's query
+                String requestBody = "{ \"query\": \"{ searchForTerm(term:\\\"" + podcastQuery + "\\\", filterForTypes:PODCASTSERIES){ searchId podcastSeries{ uuid name rssUrl } } }\" }";
+
+                // Write the JSON request to the connection's output stream
+                try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                    outputStream.writeBytes(requestBody);
+                    outputStream.flush();
+                }
+
+                // Get the response from the server
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Read the response
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+                    return response.toString();
+                } else {
+                    Log.e(TAG, "API request failed with response code: " + responseCode);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error making API request: " + e.getMessage(), e);
+            }
+            return null;
         }
 
-        return response;
+        @Override
+        protected void onPostExecute(String jsonResponse) {
+            if (jsonResponse != null) {
+                try {
+                    // Parse the JSON response
+                    JSONObject jsonObject = new JSONObject(jsonResponse);
+
+                    // Process the data as needed (e.g., extract podcast information)
+                    JSONArray podcastArray = jsonObject.getJSONObject("data")
+                            .getJSONObject("searchForTerm")
+                            .getJSONArray("podcastSeries");
+
+                    List<String> podcastNames = new ArrayList<>();
+
+                    for (int i = 0; i < podcastArray.length(); i++) {
+                        JSONObject podcastObject = podcastArray.getJSONObject(i);
+                        String podcastName = podcastObject.getString("name");
+                        podcastNames.add(podcastName);
+                    }
+
+                    // Use the extracted data in your app
+                    displayPodcasts(podcastNames);
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error parsing JSON response: " + e.getMessage(), e);
+                }
+            } else {
+                Log.e(TAG, "API response is null.");
+            }
+        }
+    }
+
+    private void displayPodcasts(List<String> podcastNames) {
+        StringBuilder names = new StringBuilder();
+        for (String name : podcastNames) {
+            names.append("Podcast Name: ").append(name).append("\n");
+        }
+        podcastNameTextView.setText(names.toString());
     }
 }
